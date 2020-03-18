93dc5484f9615a2f7ea990243a00481d33910e56
OrangeCZZKA <cdzinan@gmail.com>
2020-03-17 20:15:07 -0400

    new update

--- sentiment_split.R
+++ sentiment_split.R
@@ -1,11 +1,12 @@
 names(df1)
 try <- head(df1)
+View(try)
 
-
-library(tidyverse)
 library(tidyverse)
 library(tidyverse)
 library(readxl)
+
+
 ## new packages for us
 # install.packages("rvest")
 library(rvest)
@@ -17,6 +18,7 @@ library(wordcloud)
 library(quanteda)
 # devtools::install_github("quanteda/quanteda.corpora")
 library(quanteda.corpora)
+
 names(df1)
 
 ### split the rtweet data by 
@@ -56,6 +58,100 @@ atvi$index <- seq.int(nrow(atvi))
 
 avgo <- filter(df1, grepl("AVGO", Symbols))
 avgo$index <- seq.int(nrow(avgo))
+
+### runing sentiment for each company:
+## aal
+aal_token = aal %>% 
+  select(index, `Tweet content`) %>% 
+  unnest_tokens(token, `Tweet conte-nt`, token = "words",
+                strip_punct = T) %>% 
+  anti_join(get_stopwords(),
+            by = c("token" ="word")) %>% 
+  inner_join(get_sentiments("afinn"),
+             by = c("token" ="word")) %>% print
+### put the sentiment back to original data
+affin_aal = aal_token %>% select(index, value) %>% 
+  group_by(index) %>% 
+  summarise(polarity = mean(value)) %>% 
+  inner_join(aal)
+
+## aapl
+aapl_token = aapl %>% 
+  select(index, `Tweet content`) %>% 
+  unnest_tokens(token, `Tweet content`, token = "words",
+                strip_punct = T) %>% 
+  anti_join(get_stopwords(),
+            by = c("token" ="word")) %>% 
+  inner_join(get_sentiments("afinn"),
+             by = c("token" ="word")) %>% print
+### put the sentiment back to original data
+affin_aapl = aapl_token %>% select(index, value) %>% 
+  group_by(index) %>% 
+  summarise(polarity = mean(value)) %>% 
+  inner_join(aapl)
+
+## adbe
+adbe_token = adbe %>% 
+  select(index, `Tweet content`) %>% 
+  unnest_tokens(token, `Tweet content`, token = "words",
+                strip_punct = T) %>% 
+  anti_join(get_stopwords(),
+            by = c("token" ="word")) %>% 
+  inner_join(get_sentiments("afinn"),
+             by = c("token" ="word")) %>% print
+### put the sentiment back to original data
+affin_adbe = adbe_token %>% select(index, value) %>% 
+  group_by(index) %>% 
+  summarise(polarity = mean(value)) %>% 
+  inner_join(adbe)
+
+## adp
+adp_token = adp %>% 
+  select(index, `Tweet content`) %>% 
+  unnest_tokens(token, `Tweet content`, token = "words",
+                strip_punct = T) %>% 
+  anti_join(get_stopwords(),
+            by = c("token" ="word")) %>% 
+  inner_join(get_sentiments("afinn"),
+             by = c("token" ="word")) %>% print
+### put the sentiment back to original data
+affin_adp = adp_token %>% select(index, value) %>% 
+  group_by(index) %>% 
+  summarise(polarity = mean(value)) %>% 
+  inner_join(adp)
+
+### adsk
+adsk_token = adsk %>% 
+  select(index, `Tweet content`) %>% 
+  unnest_tokens(token, `Tweet content`, token = "words",
+                strip_punct = T) %>% 
+  anti_join(get_stopwords(),
+            by = c("token" ="word")) %>% 
+  inner_join(get_sentiments("afinn"),
+             by = c("token" ="word")) %>% print
+### put the sentiment back to original data
+affin_adsk = adsk_token %>% select(index, value) %>% 
+  group_by(index) %>% 
+  summarise(polarity = mean(value)) %>% 
+  inner_join(adsk)
+
+
+### akam
+akam_token = akam %>% 
+  select(index, `Tweet content`) %>% 
+  unnest_tokens(token, `Tweet content`, token = "words",
+                strip_punct = T) %>% 
+  anti_join(get_stopwords(),
+            by = c("token" ="word")) %>% 
+  inner_join(get_sentiments("afinn"),
+             by = c("token" ="word")) %>% print
+### put the sentiment back to original data
+affin_akam = akam_token %>% select(index, value) %>% 
+  group_by(index) %>% 
+  summarise(polarity = mean(value)) %>% 
+  inner_join(akam)
+
+
 
 
 ### alxn sentiment
