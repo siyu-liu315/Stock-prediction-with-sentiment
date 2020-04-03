8b86b8bdfebcfaf0e552ccc4d11e76d713fcee30
siyu-liu315 <53445006+siyu-liu315@users.noreply.github.com>
2020-04-03 13:56:06 -0400

    update

--- model1.R
+++ model1.R
@@ -3,7 +3,7 @@ library(caret)
 library(randomForest)
 library(readxl)
 library(tidytext)
-
+library(gbm)
 
 # ### target company for their tweets
 # ### can add more
@@ -44,6 +44,47 @@ library(tidytext)
 # group <- sentiment_data %>% group_by(symbol,Date) %>% 
 #   summarise(sum = sum(sentiment_score,na.rm = T)) %>% ungroup()
 
+
+
+## sentiment analysis
+### split the rtweet data by 
+stock <- c('AAL','AAPL',"ADBE","ADP","ADSK","AKAM",
+           "ALXN","AMAT","AMGN","AMZN","ATVI","AVGO",'')
+
+#prepare for the sentiment analysis
+df2 <- df1 %>% select(`Tweet content`,Date,RTs,Favs,Followers,Symbols)%>% 
+  separate(Symbols,into = paste('v',1:28))
+df3 <- df2 %>% pivot_longer(cols = `v 1`:`v 28`,
+                            names_to = 'pos',
+                            values_to = 'symbol',
+                            values_drop_na = T) %>% select(-pos) %>% 
+  filter(symbol %in% stock)
+
+df3$index <- seq.int(nrow(df3))
+
+### runing sentiment for each company:
+token = df3 %>% 
+  select(index, `Tweet content`) %>% 
+  unnest_tokens(token, `Tweet content`, token = "tweets",
+                strip_punct = T) %>% 
+  anti_join(get_stopwords(),
+            by = c("token" ="word")) %>% 
+  inner_join(get_sentiments("afinn"),
+             by = c("token" ="word")) %>% print
+
+token %>% select(index, value) %>% 
+  group_by(index) %>% 
+  summarise(polarity = mean(value)) %>% 
+  inner_join(df3)->sentiment_data
+
+sentiment_data$Followers <- as.numeric(sentiment_data$Followers)
+sentiment_data <- sentiment_data %>% mutate(sentiment_score = polarity * Followers/1000)
+
+group <- sentiment_data %>% group_by(symbol,Date) %>% 
+  summarise(sum = sum(sentiment_score,na.rm = T)) %>% ungroup()
+
+
+
 ### read files include finance data
 a <- read.csv('a_group.csv')
 a$date <- as.Date(as.character(a$date),format = '%m/%d/%Y')
@@ -53,7 +94,7 @@ a_finance <- a %>% select(date, TICKER,PRC,VOL) %>%
          buy = as.numeric(pct > 0 ))%>%
            filter(date >= as.Date('2016-03-10') & date <= as.Date('2016-06-15'))
 
-## EDA
+# EDA
 View(a)
 ggplot(data = a_finance, aes(x = as.factor(date), y = pct, fill = TICKER)) +
   geom_col(position = "dodge")+
@@ -66,8 +107,8 @@ ggplot(data = a_finance, aes(x = as.factor(date), y = pct, fill = TICKER)) +
 names(group) = c('TICKER','date','sentiment_score')
 group$date = as.Date(group$date)
 a_finance$buy <- as.factor(a_finance$buy)
-df_cla <- left_join(a_finance,group,by =c('date','TICKER'))             
-df_cla$sentiment_score[is.na(df_cla$sentiment_score)] <-0 
+df_cla <- left_join(a_finance,group,by =c('date','TICKER'))
+df_cla$sentiment_score[is.na(df_cla$sentiment_score)] <-0
 
 
 ### lag sentiment data
@@ -102,14 +143,14 @@ confusionMatrix(yhat.train.tree,train$buy)
 ## but not defined as a classification 
 fit_boost <- gbm(f1,
                   data = train,
-                            distribution = "bernoulli",
-                            n.trees = 5000,
+                            distribution = "gaussian",
+                            n.trees = 100,
                             interaction.depth = 2,
                             shrinkage = 0.001)
 
 pred = predict.gbm(object = fit_boost,
                    newdata = test,
-                   n.trees = 5000,
+                   n.trees = 100,
                    type = "response")
 
 confusionMatrix(round(pred),test$buy)
