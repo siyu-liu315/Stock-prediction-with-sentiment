00c0789368610525d8bc0eeab8e04e8d37de5f3e
Tyler-MSBA <53415112+Tyler-MSBA@users.noreply.github.com>
2020-04-11 03:52:11 -0400

    ok

--- portfolio_model.R
+++ portfolio_model.R
@@ -31,6 +31,10 @@ test <- model_data[-train_ind, ]
 test_real <- as.vector(test[[c(1)]])
 
 
+results <- tibble(x = 0, model = 0, mse = 0)
+
+
+
 for (x in 2:ncol(train)) {
   y <- train[c(1:x)]
   y_test <- test[c(2:x)]
@@ -39,29 +43,31 @@ for (x in 2:ncol(train)) {
   fit <- lm(y$`10/2/2019` ~ ., data = y)
   fit_predict <- predict(fit, y_test)
   mse <- mean_squared_error(test_real, fit_predict)
+  results <- results %>% add_row(x = x, model = "linear", mse = mse)
   
   ## Decision Tree
   tree <- rpart(y$`10/2/2019` ~ ., y)
   fit_predict <- predict(tree, y_test)
   mse <- mean_squared_error(test_real, fit_predict)
+  results <- results %>% add_row(x = x, model = "Decision Tree", mse = mse)
   
   ## Random Forest
   y <- clean_names(y)
   y_test <- clean_names(y_test)
-  rf <- randomForest(x10_2_2019 ~ ., y, ntree = 250, do.trace = T)
+  rf <- randomForest(x10_2_2019 ~ ., y, ntree = 2500, do.trace = F)
   fit_predict <- predict(rf, y_test)
   mse <- mean_squared_error(test_real, fit_predict)
-  print(mse)
+  results <- results %>% add_row(x = x, model = "RForest", mse = mse)
+ 
   
   ## Boosting
-  boost <- gbm(x10_2_2019 ~ .,
-                        data = y,
-                        n.trees = 250,
-                        interaction.depth = 2,
-                        shrinkage = 0.001)
-  fit_predict <- predict(boost, y_test)
-  mse <- mean_squared_error(test_real, fit_predict)
-  print(mse)
+  # boost <- gbm(x10_2_2019 ~ .,
+  #                       data = y,
+  #                       n.trees = 250,
+  #                       interaction.depth = 2,
+  #                       shrinkage = 0.001)
+  # fit_predict <- predict(boost, y_test)
+  # mse <- mean_squared_error(test_real, fit_predict)
 }
 
 for (x in 3:ncol(train)){
@@ -72,6 +78,7 @@ for (x in 3:ncol(train)){
   lasso <- cv.glmnet(y_matrix, y_train_real, alpha = 1, nfolds = 10)
   fit_predict <- predict(lasso, y_test)
   mse <- mean_squared_error(test_real, fit_predict)
+  results <- results %>% add_row(x = x, model = "Lasso", mse = mse)
 }
 
 for (x in 3:ncol(train)){
@@ -82,7 +89,8 @@ for (x in 3:ncol(train)){
   ridge <- cv.glmnet(y_matrix, y_train_real, alpha = 0, nfolds = 10)
   fit_predict <- predict(ridge, y_test)
   mse <- mean_squared_error(test_real, fit_predict)
-  print(mse)
+  results <- results %>% add_row(x = x, model = "Ridge", mse = mse)
 }
 
-
+results <- results[order(results$mse, decreasing = F),] 
+View(results)
